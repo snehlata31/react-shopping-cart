8b929c8b1de1643df98d12f82b6ff259040ddec6
Snehlata Sarode <sarode.snehlata31@gmail.com>
2021-06-16 12:03:24 +0530

    App.js & index.css Added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React shopping cart</a>
+      </header>
+      <main>
+        product list
+      </main>
+      <footer>
+        All right is reserved
+      </footer>
+    </div>
+
+  );
+}
+
+export default App;
